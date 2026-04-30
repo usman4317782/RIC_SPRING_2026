831a830f646eb939072452964a21937a51010a31
usman4317782 <usman4317782@gmail.com>
2026-04-30 08:54:49 +0500

    update

---
+++ morning/PF (ADP ITM II)/03 code (LAB)/lecture dated 30-04-2026.cpp
@@ -0,0 +1,35 @@
+#include<iostream> //header file 
+
+using namespace std;
+
+int main()
+{
+	
+//	int 	age;
+//	float	salary;
+//	char	grade;
+//	bool	isActive;
+//	string 	address;
+
+	int firstNumber, secondNumber, sum;
+	
+	cout << "Enter First Number: ";
+	cin  >> firstNumber;
+	
+	cout << "Enter Second Number: ";
+	cin  >> secondNumber;
+	
+	sum = firstNumber + secondNumber;
+	
+	cout << "Sum of " <<  firstNumber << " + "
+	<< secondNumber << " = " << sum;
+	
+	
+//	Sum of 12 + 11 = 23
+	
+	
+	
+	
+		
+	return 0;
+}

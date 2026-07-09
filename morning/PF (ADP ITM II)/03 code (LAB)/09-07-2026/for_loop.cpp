83f1113ecd29f21cef59fffdec7c6abb63081d57
usman4317782 <usman4317782@gmail.com>
2026-07-09 08:52:50 +0500

    updated

---
+++ morning/PF (ADP ITM II)/03 code (LAB)/09-07-2026/for_loop.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	//for loop 
+	//iterations
+	//table no. of student (counting)
+	//	2 4 6 8 10 2+2 = 4+2 =6
+	//1 2 3 4 
+ 	for(int i =1; i<=10; i++){
+	 if(i % 2 == 0){
+	 		cout << i << endl;
+	 }	
+	 }
+}
---
+++ morning/PF (ADP ITM II)/03 code (LAB)/09-07-2026/inc_dec_ops.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int number = 3;
+	number--;
+	cout << number;
+	//pre (increment - decrement)	
+	//post (increment - decrement)
+}
